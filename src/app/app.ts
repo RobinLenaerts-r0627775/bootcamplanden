4fe02ea57353599939148cf18c63519c4deeb447
Robin Lenaerts <contact@robinlenaerts.dev>
2026-09-21 15:16:05 +0200

    initial idea

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-root',
+  styleUrl: './app.scss',
+  templateUrl: './app.html',
+})
+export class App {
+}
